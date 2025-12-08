815186cfaf99ca0412347f538693e804da27053b
Ayush Sharma <ayushanavsharma2@gmail.com>
2025-12-08 17:07:25 +1200

    added a split_nodes image and link function.  this is similar to our delimiter BUT this operates on image and link text nodes.
    it will:
    1. scan our text node using regex for image and link bits
    
    2. use that to split our string into 2 parts (before, after)
    
    3. keep on operating on after until there are no more regex pairs that were in the returned list
    
    4. create nodes that are appropriate for the ENUM (like our delimiter)

--- src/extract_markdown_methods.py
+++ src/extract_markdown_methods.py
@@ -1,4 +1,5 @@
 import re
+from textnode import TextNode, TextType
 
 def extract_markdown_images(text):
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
@@ -7,3 +8,74 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     return re.findall(pattern, text)
+
+
+# old nodes = list of TextNodes (text, text_type, url), return [TextNode(), TextNode()]
+# text_type seems like will be TextType.TEXT
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if not node.text_type == TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        current_text = node.text
+        matches = extract_markdown_images(current_text)
+        
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        for image_text, image_url in matches:
+            fragment = f"![{image_text}]({image_url})"
+            before, after = current_text.split(fragment, 1)
+            
+            if before:
+                new_nodes.append(TextNode(before, TextType.TEXT))
+            
+            new_nodes.append(TextNode(image_text, TextType.IMAGE, image_url))
+            
+            # we are doing this because we want out AFTER to hold the text AFTER the first URL
+            current_text = after
+        
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+    
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    
+    for node in old_nodes:
+        # will use regex to match and return a list of tuples that passed [("link", "https://boot.dev")]
+        if not node.text_type == TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        current_text = node.text
+        matches = extract_markdown_links(current_text)
+        
+        # if no links, keep node as it is
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        for link_text, link_url in matches:
+            fragment = f"[{link_text}]({link_url})"
+            # only 1 split with the fragment
+            before, after = current_text.split(fragment, 1)
+            
+            if before:
+                new_nodes.append(TextNode(before, TextType.TEXT))
+            
+            # we are setting the left over text to after because we want our
+            # other links if it is in matches to go before it
+            # [before, ----links----, after]
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            
+            current_text = after
+        
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
--- src/test_extract_markdown.py
+++ src/test_extract_markdown.py
@@ -1,5 +1,6 @@
 import unittest
-from extract_markdown_methods import extract_markdown_images, extract_markdown_links
+from extract_markdown_methods import extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType
 
 class TestMDExtraction(unittest.TestCase):
     def test_images(self):
@@ -13,3 +14,33 @@ class TestMDExtraction(unittest.TestCase):
             "This is the best website to learn non-glaze pls, [bootdev](https://boot.dev)"
         )
         self.assertEqual([("bootdev", "https://boot.dev")], matches)
+        
+    def test_split_images(self):
+
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ], new_nodes
+        )
+        
+    def test_split_link(self):
+        node = TextNode("The best [website](https://boot.dev)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        
+        self.assertEqual(
+            [
+                TextNode("The best ", TextType.TEXT),
+                TextNode("website", TextType.LINK, "https://boot.dev")
+            ], new_nodes
+        )
